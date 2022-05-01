f2e9e9eee6cb188136ad576f8a60a74b151a79d6
Chad Humphries <36678974+GrunclePug@users.noreply.github.com>
2022-04-30 19:08:21 -0700

    Allow bot to call command

--- src/main/java/com/grunclepug/grunclebotupdatemanager/bot/commands/Start.java
+++ src/main/java/com/grunclepug/grunclebotupdatemanager/bot/commands/Start.java
@@ -1,6 +1,7 @@
 package com.grunclepug.grunclebotupdatemanager.bot.commands;
 
 import com.grunclepug.grunclebotupdatemanager.bot.core.Config;
+import com.grunclepug.grunclebot.bot.core.Driver;
 import com.grunclepug.grunclebotupdatemanager.bot.util.log.BotLog;
 import com.grunclepug.grunclebotupdatemanager.bot.util.manager.JarUtil;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -18,7 +19,7 @@ public class Start extends ListenerAdapter {
         String[] args = event.getMessage().getContentRaw().split("\\s+");
 
         if(args[0].equalsIgnoreCase(Config.getPrefix() + "start")) {
-            if(event.getMessage().getAuthor().getId().equals("247916497803018242")) {
+            if(event.getMessage().getAuthor().getId().equals("247916497803018242") || event.getMessage().getAuthor().getId().equals(Driver.jda.getSelfUser().getId())) {
                 try {
                     event.getMessage().getChannel().sendTyping().queue();
                     event.getMessage().getChannel().sendMessage("```fix\nBOT STARTING..\n```").queue();
